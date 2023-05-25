8e81c610e21d1ad11631f2dffc56199fff32683f
bdbrink <brendan.brink@outlook.com>
2023-05-25 17:32:44 -0500

    add save cookie

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def show_cookie():
 
     return cookie_value
 
+@app.route("/save_name", methods=["POST"])
+def save_name():
+    name = request.form['name']
+
+    response = make_response(f"we will remember your name, {name}!")
+    response.set_cookie("saved_name", name)
+    return response
+
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=9999)
